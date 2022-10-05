709047a6a69b38fe3d794d2ce9c3f15c0cf19d0f
Nalin Dahyabhai <nalin@redhat.com>
2022-10-05 17:13:09 -0400

    create: first pass
    Signed-off-by: Nalin Dahyabhai <nalin@redhat.com>

--- create.go
+++ create.go
@@ -3,6 +3,9 @@ package lukstool
 import (
 	"crypto/rand"
 	"errors"
+	"fmt"
+
+	"golang.org/x/crypto/pbkdf2"
 )
 
 func CreateV1(password []string) ([]byte, []byte, error) {
@@ -22,7 +25,6 @@ func CreateV1(password []string) ([]byte, []byte, error) {
 	if n != len(ksSalt) {
 		return nil, nil, errors.New("short read")
 	}
-
 	var h V1Header
 	h.SetMagic(V1Magic)
 	h.SetVersion(1)
@@ -33,13 +35,46 @@ func CreateV1(password []string) ([]byte, []byte, error) {
 	h.SetMKDigestSalt(salt)
 	h.SetMKDigestIter(4000)
 	h.SetUUID("")
+	mkey := make([]byte, h.KeyBytes())
+	n, err = rand.Read(mkey)
+	if err != nil {
+		return nil, nil, err
+	}
+	if n != len(mkey) {
+		return nil, nil, errors.New("short read")
+	}
+	hasher, err := hasherByName(h.HashSpec())
+	if err != nil {
+		return nil, nil, errors.New("internal error")
+	}
+	mkdigest := pbkdf2.Key(mkey, h.MKDigestSalt(), int(h.MKDigestIter()), v1DigestSize, hasher)
+	h.SetMKDigest(mkdigest)
+	headerLength := roundUpToMultiple(v1HeaderStructSize, V1AlignKeyslots)
+	var stripes [][]byte
 	for i := 0; i < 7; i++ {
 		var keyslot V1KeySlot
 		keyslot.SetActive(i < len(password))
 		keyslot.SetIterations(h.MKDigestIter())
-		keyslot.SetStripes(4000)
+		keyslot.SetStripes(h.MKDigestIter())
 		keyslot.SetKeySlotSalt(ksSalt[i*v1KeySlotSaltLength : (i+1)*v1KeySlotSaltLength])
+		if i < len(password) {
+			splitKey, err := afSplit(mkey, hasher(), int(h.MKDigestIter()))
+			if err != nil {
+				return nil, nil, fmt.Errorf("splitting key: %w", err)
+			}
+			striped, err := v1encrypt(h.CipherName(), h.CipherMode(), mkey, splitKey)
+			if err != nil {
+				return nil, nil, fmt.Errorf("encrypting split key with password: %w", err)
+			}
+			stripes = append(stripes, striped)
+			if len(striped) != len(mkey)*int(keyslot.Stripes()) {
+				return nil, nil, fmt.Errorf("internal error: got %d stripe bytes, expected %d", len(striped), len(mkey)*int(keyslot.Stripes()))
+			}
+		}
+		keyslot.SetKeyMaterialOffset(uint32(headerLength / V1SectorSize))
 		h.SetKeySlot(i, keyslot)
+		headerLength += len(mkey) * int(keyslot.Stripes())
+		headerLength = roundUpToMultiple(v1HeaderStructSize, V1AlignKeyslots)
 	}
 	return nil, nil, nil
 }
