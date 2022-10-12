7e43abd9bbe4b9d3591e21fa7f75fc9a77dc80ee
Nalin Dahyabhai <nalin@redhat.com>
2022-10-12 08:50:11 -0400

    use ivTweak in xts modes
    Signed-off-by: Nalin Dahyabhai <nalin@redhat.com>

--- encryption.go
+++ encryption.go
@@ -76,7 +76,7 @@ func v1encrypt(cipherName, cipherMode string, ivTweak int, key []byte, plaintext
 			if processed+blockLeft > len(plaintext) {
 				blockLeft = len(plaintext) - processed
 			}
-			cipher.Encrypt(ciphertext[processed:processed+blockLeft], plaintext[processed:processed+blockLeft], uint64(processed/V1SectorSize))
+			cipher.Encrypt(ciphertext[processed:processed+blockLeft], plaintext[processed:processed+blockLeft], uint64(processed/V1SectorSize+ivTweak))
 		}
 	default:
 		return nil, fmt.Errorf("unsupported cipher mode %s", cipherMode)
@@ -145,7 +145,7 @@ func v1decrypt(cipherName, cipherMode string, ivTweak int, key []byte, ciphertex
 			if processed+blockLeft > len(ciphertext) {
 				blockLeft = len(ciphertext) - processed
 			}
-			cipher.Decrypt(plaintext[processed:processed+blockLeft], ciphertext[processed:processed+blockLeft], uint64(processed/V1SectorSize))
+			cipher.Decrypt(plaintext[processed:processed+blockLeft], ciphertext[processed:processed+blockLeft], uint64(processed/V1SectorSize+ivTweak))
 		}
 	default:
 		return nil, fmt.Errorf("unsupported cipher mode %s", cipherMode)
